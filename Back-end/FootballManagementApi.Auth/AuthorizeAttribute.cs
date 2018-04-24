34b76e62bc2abdbc1c952d39d65fdb8a0d907dbb
IIMullahmetov <IIMullahmetov@gmail.com>
2018-04-24 23:09:48 +0300

    Добавил либы для owin

--- Back-end/FootballManagementApi.Auth/AuthorizeAttribute.cs
+++ Back-end/FootballManagementApi.Auth/AuthorizeAttribute.cs
@@ -3,10 +3,12 @@ using System.Threading.Tasks;
 using System.Web.Http.Controllers;
 using System.Web.Http.Filters;
 using FootballManagementApi.Enums;
+using System.Net.Http;
+using Microsoft.Owin;
 
 namespace FootballManagementApi.Auth
 {
-    public class AuthorizeAttribute : ActionFilterAttribute
+	public class AuthorizeAttribute : ActionFilterAttribute
     {
         private Role? _role;
 
@@ -16,10 +18,12 @@ namespace FootballManagementApi.Auth
         {
             _role = role;
         }
+	
+		public override async Task OnActionExecutingAsync(HttpActionContext actionContext, CancellationToken cancellationToken)
+		{
+			IOwinContext context = actionContext.Request.GetOwinContext();
 
-        public override async Task OnActionExecutingAsync(HttpActionContext actionContext, CancellationToken cancellationToken)
-        {
-            actionContext.RequestContext.GetOwinContext();
+			
         }
         
     }
