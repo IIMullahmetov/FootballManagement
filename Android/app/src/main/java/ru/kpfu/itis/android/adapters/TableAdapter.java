712de259a1cf7f029aa407d2c0a4921f1b0f4407
Motygullin503 <bul3515@gmail.com>
2018-04-24 23:37:59 +0300

    Championship Table

---
+++ Android/app/src/main/java/ru/kpfu/itis/android/adapters/TableAdapter.java
@@ -0,0 +1,92 @@
+package ru.kpfu.itis.android.adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.Collections;
+import java.util.List;
+
+import ru.kpfu.itis.android.R;
+import ru.kpfu.itis.android.models.TableItem;
+
+/**
+ * Created by Bulat on 24.04.2018 at 20:12.
+ */
+public class TableAdapter extends RecyclerView.Adapter<TableAdapter.TableViewHolder> {
+    private Context context;
+    private List<TableItem> tables;
+
+    public TableAdapter(Context context) {
+        this.context = context;
+        tables = Collections.emptyList();
+    }
+
+    @NonNull
+    @Override
+    public TableAdapter.TableViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.table_item, parent, false);
+        return new TableAdapter.TableViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TableAdapter.TableViewHolder holder, int position) {
+        final TableItem tableItem = tables.get(position);
+        Glide.with(context).load(R.drawable.real_madrid)
+                .apply(RequestOptions.fitCenterTransform())
+                .into(holder.ivLogo);
+
+        holder.tvName.setText(tableItem.getTeamName());
+        holder.tvGames.setText(tableItem.getGames());
+        holder.tvWins.setText(tableItem.getWins());
+
+        holder.tvDraws.setText(tableItem.getDraws());
+        holder.tvLosses.setText(tableItem.getLosses());
+        holder.tvPoints.setText(tableItem.getPoints());
+    }
+
+    @Override
+    public int getItemCount() {
+        return tables.size();
+    }
+
+    public void setTable(List<TableItem> tables) {
+        this.tables = tables;
+        notifyDataSetChanged();
+    }
+
+    public class TableViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView ivLogo;
+        private TextView tvName;
+        private TextView tvGames;
+        private TextView tvWins;
+        private TextView tvDraws;
+        private TextView tvLosses;
+        private TextView tvPoints;
+
+
+        public TableViewHolder(View itemView) {
+            super(itemView);
+            ivLogo = itemView.findViewById(R.id.logo);
+            tvName = itemView.findViewById(R.id.team);
+            tvGames = itemView.findViewById(R.id.games);
+            tvWins = itemView.findViewById(R.id.wins);
+            tvDraws = itemView.findViewById(R.id.draws);
+            tvLosses = itemView.findViewById(R.id.losses);
+            tvPoints = itemView.findViewById(R.id.points);
+
+
+        }
+    }
+
+
+}
---
+++ Android/app/src/main/java/ru/kpfu/itis/android/models/TableItem.java
@@ -0,0 +1,90 @@
+package ru.kpfu.itis.android.models;
+
+/**
+ * Created by Bulat on 24.04.2018 at 19:34.
+ */
+public class TableItem {
+
+
+
+    private String icon;
+
+    private String teamName;
+
+    private String games;
+
+    private String wins;
+
+    private String draws;
+
+    private String losses;
+
+    private String points;
+
+    public TableItem(String icon, String teamName, String games, String wins, String draws, String losses, String points) {
+
+        this.icon = icon;
+        this.teamName = teamName;
+        this.games = games;
+        this.wins = wins;
+        this.draws = draws;
+        this.losses = losses;
+        this.points = points;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public String getGames() {
+        return games;
+    }
+
+    public void setGames(String games) {
+        this.games = games;
+    }
+
+    public String getWins() {
+        return wins;
+    }
+
+    public void setWins(String wins) {
+        this.wins = wins;
+    }
+
+    public String getDraws() {
+        return draws;
+    }
+
+    public void setDraws(String draws) {
+        this.draws = draws;
+    }
+
+    public String getLosses() {
+        return losses;
+    }
+
+    public void setLosses(String losses) {
+        this.losses = losses;
+    }
+
+    public String getPoints() {
+        return points;
+    }
+
+    public void setPoints(String points) {
+        this.points = points;
+    }
+}
