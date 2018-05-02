583ab8c9ff800078454005497dd90412389b1590
Reenaz <sirazievrinaz@gmail.com>
2018-05-02 20:04:04 +0300

    del some default code

--- Front-end/React app/football_app/src/pages/App.js
+++ Front-end/React app/football_app/src/pages/App.js
@@ -3,25 +3,13 @@ import history from 'utils/history';
 import { Provider } from 'react-redux';
 import { Router, Route } from 'react-router-dom';
 
-import Limits from 'components/modals/Limits';
-import Login from 'components/modals/Login';
-import Sms from 'components/modals/Sms';
-import Error from 'components/modals/Error';
-import CreateCounter from 'components/modals/CreateCounter/';
-import CounterAbout from 'components/modals/CounterAbout/';
-import Alert from 'components/modals/Alert';
-import Confirm from 'components/modals/Confirm';
 
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 
 // import Permission from 'components/Permission';
 
-import Home from './home';
-import Entrance from './entrance';
-import Devices from './devices';
-import Statistic from './statistic';
-import Auth from './auth';
+
 
 import './app.css';
 
@@ -32,7 +20,7 @@ import './app.css';
 const App = ({ store }: { store: Object }) => (
   <div>
     <Provider store={store}>
-      <Router basename="/abdev" history={history}>
+      <Router basename="/" history={history}>
         <div className="container">
           {/* <Route path="/" exact render={() => <Redirect to="/login" />} /> */}
           <Route
@@ -49,14 +37,7 @@ const App = ({ store }: { store: Object }) => (
                 <Alert />
               </div>
             )}
-          />
-          <Route path="/operator" component={Header} />
-          <Route path="/login" component={Auth} />
-          <Route path="/operator/home" component={Home} />
-          <Route path="/operator/entrance" component={Entrance} />
-          <Route path="/operator/devices" component={Devices} />
-          <Route path="/operator/statistic" component={Statistic} />
-          <Route path="/" component={Footer} />
+          />        
         </div>
       </Router>
     </Provider>
