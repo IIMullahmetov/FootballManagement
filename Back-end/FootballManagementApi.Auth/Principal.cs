8ff434dd7f78c0d1932ceecc54acf0cace166d6e
Ильяс Муллахметов <IIMullahmetov@gmail.com>
2018-05-17 16:54:20 +0300

    merge

--- Back-end/FootballManagementApi.Auth/Principal.cs
+++ Back-end/FootballManagementApi.Auth/Principal.cs
@@ -4,11 +4,9 @@ namespace FootballManagementApi.Auth
 {
     public class Principal : IPrincipal
     {
-        private readonly DateTime _startDt = new DateTime(2018, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-
         public Principal(Jwt jwt)
         {
-            ExpireAt = _startDt.AddSeconds(jwt.ExpireAt);
+            ExpireAt = jwt.ExpireAt;
             Identity = new Identity
             {
                 Id = jwt.Id,
